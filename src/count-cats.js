7d7b64dba7358750bdde4d59f4bee4d7767d189f
olenaweb <olena.nevzorova@gmail.com>
2023-12-25 19:57:44 +0200

    20 pass

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
 
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
+  if (matrix == []) { return 0; }
+  let maxRow = matrix.length;
+  // console.log('maxRow = ', maxRow);
+  // console.log('maxCol = ', maxCol);
+  let count = 0;
+  for (let row = 0; row < maxRow; row++) {
+    let maxCol = matrix[row].length;
+    for (let col = 0; col < maxCol; col++) {
+      if (matrix[row][col] === '^^') { count++; };
+    }
+  }
+  return count;
+}
 module.exports = {
   countCats
 };
